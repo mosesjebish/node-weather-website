274999c938bd4f390842335fd4804f4193e2e4e9
jebishmoses <jebish.moses@thoughtworks.com>
2021-05-28 18:31:16 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+// console.log('Client side javascript file is loaded')
+
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1') // .className | #idName | tagname
+const messageTwo = document.querySelector('#message-2') // .className | #idName | tagname
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                return
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,31 @@
+const request = require('request')
+
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5b291e024b2c9f58db2fa8b3b88d1723&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to forecast services!', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            const currentWeather = body.current
+            callback(undefined, currentWeather.weather_descriptions[0] + '. It is currently ' + currentWeather.temperature + ' degrees out. It feels like ' + currentWeather.feelslike + ' out')
+        }
+    })
+}
+
+module.exports = forecast
